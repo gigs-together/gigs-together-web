f9f8979a9728fbdb4f0f53c789160662a2a09db4
Peter Vysotskiy <vysotskiy.petr@gmail.com>
2024-12-07 16:44:03 +0400

    Tweak UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,57 @@ import { Separator } from '@/components/ui/separator';
 import { ToggleGroup, ToggleGroupItem } from '@/components/ui/toggle-group';
 import styled from 'styled-components';
 
-export function DaySection({ children, title }: { children: React.ReactNode, title: string }) {
+import './style.css';
+import { useEffect, useRef } from 'react';
+
+type DaySectionProps = {
+  children: React.ReactNode,
+  title: string,
+  date: string,
+}
+
+export function DaySection({ children, title, date }: DaySectionProps) {
+  const titleRef = useRef<HTMLDivElement>(null);
+  const dayRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const titleElement = titleRef.current;
+    if (!titleElement) return;
+
+    const root = document.querySelector('main');
+    if (!root) return;
+
+    const dayObserver = new IntersectionObserver(
+      ([entry]) => {
+        const isOffscreen = entry.boundingClientRect.y + entry.boundingClientRect.height < entry.rootBounds!.top!;
+        dayRef.current!.classList.toggle('day-offscreen', isOffscreen);
+
+        const isSticky = entry.rootBounds!.top >= entry.boundingClientRect.top - 5;
+        titleElement.classList.toggle('day-title-sticky', isSticky);
+      },
+      { threshold: [0, 0.0001, 0.9999, 1],
+        root,
+      }
+    );
+
+    dayObserver.observe(dayRef.current!);
+
+    return () => {
+      dayObserver.unobserve(dayRef.current!);
+    };
+  }, []);
+
+
   return (
-    <div className="flex flex-col w-full pl-12">
-      <h3 className="text-2xl font-bold text-gray-600 sticky">{title}</h3>
-      <Separator orientation="horizontal" className="mb-4 h-1 bg-gray-600" />
+    <div id={'day-' + date} ref={dayRef} className="flex flex-col w-full pl-16">
+      <div className="day-title-container">
+        <div ref={titleRef} className="day-title">
+          <h3 className="text-2xl font-bold text-gray-600 sticky top-0">{title}</h3>
+          <Separator orientation="horizontal" className="mb-3 h-1 bg-gray-500" />
+        </div>
+      </div>
       <div className="flex-wrap flex flex-row gap-4">
-      {children}
+        {children}
       </div>
     </div>
   )
@@ -48,34 +92,138 @@ const StyledToggleGroup = styled(ToggleGroup)`
   flex-direction: column;
 `
 
-const days = ["9", "10", "11", "12", "13", "14", "15", "22", "29"]
+const StyledToggleGroupItem = styled(ToggleGroupItem)`
+  font-size: 0.8rem;
+  width: 2.5rem;
+  height: 2.5rem;
+  border-radius: 50%;
+  border: 1px solid #a0a0a0;
+  color: #a0a0a0;
+`
+
+const events = [
+  {
+    date: "2024-12-09",
+    cover: "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F905877343%2F2407230629043%2F1%2Foriginal.20241125-122702?crop=focalpoint&fit=crop&w=512&auto=format%2Ccompress&q=75&sharp=10&fp-x=0.5&fp-y=0.5&s=15e34abb8d4071b78436f10e3e5eae7a",
+    title: "Awesome concert",
+    people: 100
+  },
+  {
+    date: "2024-12-09",
+    cover: "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F905877343%2F2407230629043%2F1%2Foriginal.20241125-122702?crop=focalpoint&fit=crop&w=512&auto=format%2Ccompress&q=75&sharp=10&fp-x=0.5&fp-y=0.5&s=15e34abb8d4071b78436f10e3e5eae7a",
+    title: "Awesome concert",
+    people: 100
+  },
+  {
+    date: "2024-12-09",
+    cover: "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F905877343%2F2407230629043%2F1%2Foriginal.20241125-122702?crop=focalpoint&fit=crop&w=512&auto=format%2Ccompress&q=75&sharp=10&fp-x=0.5&fp-y=0.5&s=15e34abb8d4071b78436f10e3e5eae7a",
+    title: "Awesome concert",
+    people: 100
+  },
+  {
+    date: "2024-12-10",
+    cover: "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F905877343%2F2407230629043%2F1%2Foriginal.20241125-122702?crop=focalpoint&fit=crop&w=512&auto=format%2Ccompress&q=75&sharp=10&fp-x=0.5&fp-y=0.5&s=15e34abb8d4071b78436f10e3e5eae7a",
+    title: "Awesome concert",
+    people: 100
+  },
+  {
+    date: "2024-12-10",
+    cover: "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F905877343%2F2407230629043%2F1%2Foriginal.20241125-122702?crop=focalpoint&fit=crop&w=512&auto=format%2Ccompress&q=75&sharp=10&fp-x=0.5&fp-y=0.5&s=15e34abb8d4071b78436f10e3e5eae7a",
+    title: "Awesome concert",
+    people: 100
+  },
+  {
+    date: "2024-12-10",
+    cover: "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F905877343%2F2407230629043%2F1%2Foriginal.20241125-122702?crop=focalpoint&fit=crop&w=512&auto=format%2Ccompress&q=75&sharp=10&fp-x=0.5&fp-y=0.5&s=15e34abb8d4071b78436f10e3e5eae7a",
+    title: "Awesome concert",
+    people: 100
+  },
+  {
+    date: "2024-12-10",
+    cover: "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F905877343%2F2407230629043%2F1%2Foriginal.20241125-122702?crop=focalpoint&fit=crop&w=512&auto=format%2Ccompress&q=75&sharp=10&fp-x=0.5&fp-y=0.5&s=15e34abb8d4071b78436f10e3e5eae7a",
+    title: "Awesome concert",
+    people: 100
+  },
+  {
+    date: "2024-12-11",
+    cover: "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F905877343%2F2407230629043%2F1%2Foriginal.20241125-122702?crop=focalpoint&fit=crop&w=512&auto=format%2Ccompress&q=75&sharp=10&fp-x=0.5&fp-y=0.5&s=15e34abb8d4071b78436f10e3e5eae7a",
+    title: "Awesome concert",
+    people: 100
+  },
+  {
+    date: "2024-12-11",
+    cover: "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F905877343%2F2407230629043%2F1%2Foriginal.20241125-122702?crop=focalpoint&fit=crop&w=512&auto=format%2Ccompress&q=75&sharp=10&fp-x=0.5&fp-y=0.5&s=15e34abb8d4071b78436f10e3e5eae7a",
+    title: "Awesome concert",
+    people: 100
+  },
+  {
+    date: "2024-12-11",
+    cover: "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F905877343%2F2407230629043%2F1%2Foriginal.20241125-122702?crop=focalpoint&fit=crop&w=512&auto=format%2Ccompress&q=75&sharp=10&fp-x=0.5&fp-y=0.5&s=15e34abb8d4071b78436f10e3e5eae7a",
+    title: "Awesome concert",
+    people: 100
+  },
+  {
+    date: "2024-12-12",
+    cover: "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F905877343%2F2407230629043%2F1%2Foriginal.20241125-122702?crop=focalpoint&fit=crop&w=512&auto=format%2Ccompress&q=75&sharp=10&fp-x=0.5&fp-y=0.5&s=15e34abb8d4071b78436f10e3e5eae7a",
+    title: "Awesome concert",
+    people: 100
+  },
+  {
+    date: "2024-12-12",
+    cover: "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F905877343%2F2407230629043%2F1%2Foriginal.20241125-122702?crop=focalpoint&fit=crop&w=512&auto=format%2Ccompress&q=75&sharp=10&fp-x=0.5&fp-y=0.5&s=15e34abb8d4071b78436f10e3e5eae7a",
+    title: "Awesome concert",
+    people: 100
+  },
+  {
+    date: "2024-12-12",
+    cover: "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F905877343%2F2407230629043%2F1%2Foriginal.20241125-122702?crop=focalpoint&fit=crop&w=512&auto=format%2Ccompress&q=75&sharp=10&fp-x=0.5&fp-y=0.5&s=15e34abb8d4071b78436f10e3e5eae7a",
+    title: "Awesome concert",
+    people: 100
+  }
+];
+
+const eventsByDate: Record<string, typeof events> = {};
+
+events.forEach((event) => {
+  const date = event.date;
+  eventsByDate[date] = eventsByDate[date] || [];
+  eventsByDate[date].push(event);
+});
+
+const days = Object.keys(eventsByDate).sort().map((date) => {
+  return {
+    date: date,
+    events: eventsByDate[date],
+  }
+});
 
 export default function Home() {
+
+  const handleDayChange = (value: string) => {
+    document.getElementById('day-' + value)?.scrollIntoView({ behavior: 'smooth' });
+  }
+
   return (
     <div className={styles.page}>
       <Header/>
       <main className={styles.main}>
-        <div className="flex flex-col items-center gap-4 p-1 border-radius-lg border-gray-200 fixed top-0 bg-white left-1 top-32">
-          <strong>Dec</strong>
-          <StyledToggleGroup type="single" orientation="vertical">
+        <div className="flex flex-col items-center gap-1 p-1 border-radius-lg border-gray-200 fixed top-0 bg-white left-0 top-32 w-16">
+          <strong className="bg-black text-white uppercase w-10 h-10 rounded-full flex items-center justify-center">Dec</strong>
+          <StyledToggleGroup type="single" orientation="vertical" onValueChange={handleDayChange}>
             {days.map((day) => (
-              <ToggleGroupItem key={day} value={day} aria-label={day}>
-                {day}
-              </ToggleGroupItem>
+              <StyledToggleGroupItem key={day.date} value={day.date} aria-label={day.date}>
+                {new Date(day.date).getDate()}
+              </StyledToggleGroupItem>
             ))}
           </StyledToggleGroup>
         </div>
-        <DaySection title='December 9'>
-          <Card cover="https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F905877343%2F2407230629043%2F1%2Foriginal.20241125-122702?crop=focalpoint&fit=crop&w=512&auto=format%2Ccompress&q=75&sharp=10&fp-x=0.5&fp-y=0.5&s=15e34abb8d4071b78436f10e3e5eae7a"/>
-          <Card cover="https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F905877343%2F2407230629043%2F1%2Foriginal.20241125-122702?crop=focalpoint&fit=crop&w=512&auto=format%2Ccompress&q=75&sharp=10&fp-x=0.5&fp-y=0.5&s=15e34abb8d4071b78436f10e3e5eae7a"/>
-          <Card cover="https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F905877343%2F2407230629043%2F1%2Foriginal.20241125-122702?crop=focalpoint&fit=crop&w=512&auto=format%2Ccompress&q=75&sharp=10&fp-x=0.5&fp-y=0.5&s=15e34abb8d4071b78436f10e3e5eae7a"/>
-        </DaySection>
-        <DaySection title='December 10'>
-          <Card cover="https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F901371753%2F2504186999801%2F1%2Foriginal.20241118-171027?w=512&auto=format%2Ccompress&q=75&sharp=10&rect=0%2C20%2C2378%2C1189&s=78c0370bd50f40cd5373c11da5994e57"/>
-          <Card cover="https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F901371753%2F2504186999801%2F1%2Foriginal.20241118-171027?w=512&auto=format%2Ccompress&q=75&sharp=10&rect=0%2C20%2C2378%2C1189&s=78c0370bd50f40cd5373c11da5994e57"/>
-          <Card cover="https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F901371753%2F2504186999801%2F1%2Foriginal.20241118-171027?w=512&auto=format%2Ccompress&q=75&sharp=10&rect=0%2C20%2C2378%2C1189&s=78c0370bd50f40cd5373c11da5994e57"/>
-          <Card cover="https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F901371753%2F2504186999801%2F1%2Foriginal.20241118-171027?w=512&auto=format%2Ccompress&q=75&sharp=10&rect=0%2C20%2C2378%2C1189&s=78c0370bd50f40cd5373c11da5994e57"/>
-        </DaySection>
+        {days.map((day) => (
+          <DaySection key={day.date} title={day.date} date={day.date}>
+            {day.events.map((event) => (
+              <Card key={event.title} cover={event.cover} />
+            ))}
+          </DaySection>
+        ))}
       </main>
     </div>
   );
