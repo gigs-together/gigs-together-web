1a55ee9ab17a404c43503ebb12f15d76d493aeb3
Peter Vysotskiy <vysotskiy.petr@gmail.com>
2025-06-24 18:04:40 +0400

    Take off private connection string

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://g2g:mYymK9tOErdVF2K7@cluster0.tjzzw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+const MONGODB_URI = process.env.MONGODB_URI || '';
 
 if (!MONGODB_URI) {
   throw new Error(
