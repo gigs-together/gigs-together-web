be5d4ba07b831a1dada82b2440316790f1705c12
Arina M <thearinamarr@gmail.com>
2025-09-06 23:31:06 +0200

    fix: lint-staged.config.js

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -3,6 +3,6 @@
  * @type {import('lint-staged').Configuration}
  */
 export default {
-  '*.{js,ts,jsx,tsx}': ['eslint --fix', 'prettier --write'],
+  '*.{js,ts,jsx,tsx}': ['prettier --write', 'eslint --fix'],
   '*.{json,md,css,scss,yml,yaml}': ['prettier --write'],
 };
